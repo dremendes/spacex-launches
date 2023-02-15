11f9271664b3eb6fd0e701ffa6ddc3c19749e925
André Mendes <andre@payhop.com.br>
2023-02-15 19:53:31 -0300

    Replaces GraphQL server and data models

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,10 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const API_URL = 'https://api.spacex.land/graphql/';
+const API_URL = 'https://spacex-production.up.railway.app/';
 
 const client = new ApolloClient({
   uri: API_URL,
   cache: new InMemoryCache(),
 });
+
+console.log(client);
 
 export default client;
